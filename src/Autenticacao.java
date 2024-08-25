7619590efb8deb640206ab82df24f086f6fd2e6a
jusoaresg <julianosgreg@gmail.com>
2024-08-25 11:55:35 -0300

    feat: adicionado classe Autenticacao
    Criada classe autenticacao de clientes
    
    	Map de Cpf e cliente para clientes registrados
    
    	Classe registrarClientes com verificação se o cliente já existe
    
    	Classe login com verificação se o cliente esta registrado ou se
    	a senha esta correta
    
    Classe ainda não funcional sem a criação da classe Cliente

---
+++ src/Autenticacao.java
@@ -0,0 +1,42 @@
+import java.util.Map;
+
+public class Autenticacao {
+
+	// <Cpf, Cliente obj>
+	Map<String, Cliente> clientesRegistrados = new HashMap<String, Cliente>();
+
+	public Cliente registrarCliente(String nome, String cpf, String email, String password, String endereco) {
+
+		Cliente cliente = new Cliente(nome, cpf, email, password, endereco);
+
+		if (clientesRegistrados.containsKey(cpf)) {
+			System.out.println("Cliente já registrado nesse cpf");
+			return null;
+		}
+
+		clientesRegistrados.put(cliente.cpf, cliente);
+
+		System.out.println("Cliente criado com sucesso");
+		return cliente;
+
+	}
+
+	public Cliente login(String cpf, String password) {
+
+		Cliente cliente = clientesRegistrados.get(cpf);
+
+		if (cliente == null) {
+			System.out.println("Cliente não registrado");
+			return null;
+		}
+
+		if (cliente.password != password) {
+			System.out.println("Senha errada, tente novamente");
+			return null;
+		}
+
+		System.out.println("Cliente logado com sucesso");
+		return cliente;
+	}
+
+}
