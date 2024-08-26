9bea64d0c9263ddc67d4bf9e21abb2703423920b
jusoaresg <julianosgreg@gmail.com>
2024-08-26 19:04:56 -0300

    feat: adicionado classe cliente

---
+++ src/Cliente.java
@@ -0,0 +1,42 @@
+import java.util.List;
+
+public class Cliente {
+	private String nomeCliente;
+	private String cpf;
+	private String email;
+	private String password;
+	private String endereco;
+
+	private List<Cartao> cartoes;
+	private List<ContaBancaria> contas;
+	private List<Emprestimos> emprestimos;
+
+	// Construtor para inicializacao dos atributos
+	public Cliente(String nomeCliente, String cpf, String email, String password, String endereco) {
+		this.nomeCliente = nomeCliente;
+		this.cpf = cpf;
+		this.email = email;
+		this.password = password;
+		this.endereco = endereco;
+	}
+
+	// metodo para retornar o nome do cliente
+	public String getNome() {
+		return nomeCliente;
+	}
+
+	// metodo para retornar as contas
+	public List<ContaBancaria> getContas() {
+		return contas;
+	}
+
+	// metodo para abertura de novas contas
+	public void abrirConta(ContaBancaria novaConta) {
+		contas.add(novaConta);
+	}
+
+	// metodo para emitir cartoes
+	public void emitirCartao(Cartao novoCartao) {
+		cartoes.add(novoCartao);
+	}
+}
